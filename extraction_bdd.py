ba4cd90229ab96b833f747d18aeafee6c2575072
mwebecl <matthieu.weber@ecl16.ec-lyon.fr>
2017-05-29 09:33:47 +0200

    ajout fonction moyenne

--- extraction_bdd.py
+++ extraction_bdd.py
@@ -20,12 +20,42 @@ def get_data(id_station):
     q_tp=[]
     
     c.execute("""SELECT * FROM temperatures ORDER BY date""")
-    for line in c.execute("""SELECT date,tp,q_tp FROM temperatures WHERE id=33"""):
+    for line in c.execute("""SELECT date,tp,q_tp FROM temperatures WHERE id={}""".format(id_station)):
         date.append(line[0])
         tp.append(line[1])
         q_tp.append(line[2])
         
     return date,tp,q_tp
     
+
+def moyenne_annee(date,tp,q_tp):
     
+    n=len(date)
+    d=0
+    current_year=str(date[0])[0:4]
+    current_mean=0
+    current_nb=0
+    annee=[]
+    tp_annee=[]
+    
+    for d in range(n):
+        if q_tp[d]==0:
+            if str(date[d])[0:4]==current_year:
+                current_mean+=tp[d]
+                current_nb+=1
+            else:
+                annee.append(int(current_year))
+                tp_annee.append(current_mean/current_nb)
+                current_year=str(date[d])[0:4]
+                current_nb=1
+                current_mean=tp[d]
+        
+    annee.append(int(current_year))
+    tp_annee.append(current_mean/current_nb)  
+     
+    return annee,tp_annee
+
+#date,tp,q_tp=get_data(33)    
+#print(moyenne_annee(date,tp,q_tp))    
+        
   #coupe la connection
