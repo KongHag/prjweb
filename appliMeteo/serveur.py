f9f1097a1c05726b90ffc1ed86c708fa919fb860
mwebecl <matthieu.weber@ecl16.ec-lyon.fr>
2017-05-29 11:10:55 +0200

    a

--- appliMeteo/serveur.py
+++ appliMeteo/serveur.py
@@ -7,7 +7,7 @@ import socketserver
 from urllib.parse import urlparse, parse_qs
 import json
 from extraction_bdd import *
-
+from affiche_graphique import *
 # définition du handler
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
@@ -34,13 +34,8 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
     # requete description - retourne la description du lieu dont on passe l'id en paramètre dans l'URL
     elif self.path_info[0] == "description":
-      data=[{'id':1,'desc':"Il ne faut pas être <b>trop grand</b> pour marcher dans cette rue qui passe sous une maison"},
-            {'id':2,'desc':"Cette rue est <b>si étroite</b> qu'on touche les 2 côtés en tendant les bras !"},
-            {'id':3,'desc':"Ce jardin <b>méconnu</b> évoque le palais idéal du Facteur Cheval"}]
-      for c in data:
-        if c['id'] == int(self.path_info[1]):
-          self.send_json(c)
-          break
+      
+      genere_graphe_id(int(self.path_info[1])
 
     # requête générique
     elif self.path_info[0] == "service":
