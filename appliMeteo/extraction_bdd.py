4f88c1e399483e2f83a8f9a3d8ab49f3627fee9d
Tasty <colin.cros@gmail.com>
2017-05-29 10:40:16 +0200

    affichage

--- appliMeteo/extraction_bdd.py
+++ appliMeteo/extraction_bdd.py
@@ -27,7 +27,29 @@ def get_data(id_station):
         
     return date,tp,q_tp
     
-
+def get_station():
+    
+    c=conn.cursor()
+    
+    id_station=[]
+    nom=[]
+    lat=[]
+    lon=[]
+    alt=[]
+    
+    for line in c.execute("""SELECT * FROM stations_meteo"""):
+        id_station.append(line[0])
+        nom.append(line[1])
+        lat.append(min_to_dec(line[2]))
+        lon.append(min_to_dec(line[3]))
+        alt.append(line[4])
+        
+    return id_station,nom,lat,lon,alt
+    
+def min_to_dec(a):
+    [deg,minu,sec]=a.split(':')
+    return int(deg)+int(minu)/60+int(sec)/3600
+        
 def moyenne_annee(date,tp,q_tp):
     
     n=len(date)
@@ -58,5 +80,4 @@ def moyenne_annee(date,tp,q_tp):
 
 #date,tp,q_tp=get_data(742)    
 #print(moyenne_annee(date,tp,q_tp))    
-        
- #coupe la connection
+     
